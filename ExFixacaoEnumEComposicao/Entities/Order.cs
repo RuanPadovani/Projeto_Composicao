745151d99d2dea640b28d1f08d778835aba527ba
Ruan <ruanpadovani09@gmail.com>
2025-05-08 16:37:21 -0300

    Primeiro commit

---
+++ ExFixacaoEnumEComposicao/Entities/Order.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using ExFixacaoEnumEComposicao.Entities.Enums;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ExFixacaoEnumEComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public Product Product { get; set; }
+        public OrderItem OrderItem { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(Client client, OrderStatus status) 
+        {
+            Client = client;
+            Status = status;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+
+        }
+
+        public void RemoveItem(OrderItem item) 
+        { 
+            Items.Remove(item); 
+        }
+
+        public double Total() 
+        {
+            double sum = 0;
+
+            foreach (OrderItem item in Items) 
+            {
+                sum += item.SubTotal();
+
+            }
+        
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY:");
+            sb.AppendLine("Order moment: " + Date.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client.Name + " (" + Client.BirthDate.ToString("dd/MM/yyyy") + ") - " + Client.Email);
+            sb.AppendLine("Order items:");
+
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.Product.Name
+                    + ", $" + item.Price.ToString("F2")
+                    + ", Quantity: " + item.Quantity
+                    + ", Subtotal: $" + item.SubTotal().ToString("F2"));
+            }
+
+            sb.AppendLine("Total price: $" + Total().ToString("F2"));
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ ExFixacaoEnumEComposicao/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using ExFixacaoEnumEComposicao.Entities;
+using ExFixacaoEnumEComposicao.Entities.Enums;
+
+namespace ProjectEnumEComp
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birthdate (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email,birthDate);   
+            Order order = new Order(client, status);
+
+            Console.WriteLine("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine($"Enter #{i+1} item data:");
+                Console.Write("Product Name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Product Price: ");
+                double priceProduct = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(nameProduct, priceProduct);
+                OrderItem item = new OrderItem(quantity, priceProduct,product);
+
+                order.AddItem(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(order.ToString());
+
+
+        }
+    }
+}
+
